d5f2e0f146ba9fae9e6f89659ca353bbe7cc3379
Kelly Zheng <kellywzheng@gmail.com>
2021-02-08 17:06:00 -0800

    Update App.js to include Login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Logo from './components/Logo.js'
 import Menu from './navbar2.js';
+import Login from './login.js'; 
 
 function App() {
   return (
      <div>
-         <div className="float-child">
-             <Menu />
-         </div>
         <div className="App">
           <Logo />
+        </div>
+        <div>
+            <Menu />
+        </div>
+        <div>
+            <Login />
         </div>
     </div>
   );
