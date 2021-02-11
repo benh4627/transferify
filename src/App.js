e73ba7fca8a5c248c89746bb2beb7b53a6a05257
latishasumardy <latishasumardy@gmail.com>
2021-02-10 23:50:00 -0800

    static classcard

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 import Menu from './components/navbar2.js';
 import Home from './components/Home.js';
+import ClassCard from './classcard.js';
 import './App.css'
 
 function App() {
     return(
-        <div className="App">
-            <div className="HomeModule"><Home /></div>
+        <div>
+            <ClassCard />
         </div>
     );
 }
 
 export default App;
+
+{/*<div className="App">
+    <div className="HomeModule"><Home /></div>
+</div>*/}
--- src/classcard.js
+++ src/classcard.js
@@ -1,4 +1,27 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
+import React, { Component } from 'react';
 import Button from "react-bootstrap/Button";
+import {Card} from 'react-bootstrap';
+import {ListGroup} from 'react-bootstrap';
 import "./classcard.css";
+
+function ClassCard() {
+    return(
+        <Card style={{ width: '40rem' , font-size: 50rem,}}>
+            <Card.Header>Class Name</Card.Header>
+            <Card.Body>
+                <div className="description">
+                    <Card.Subtitle>Graduation Requirement Type</Card.Subtitle>
+                    <Card.Text>Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</Card.Text>
+                </div>
+                <Card.Subtitle>Prerequisites:</Card.Subtitle>
+                <ListGroup variant="flush">
+                    <ListGroup.Item>Prereq 1</ListGroup.Item>
+                    <ListGroup.Item>Prereq 2</ListGroup.Item>
+                </ListGroup>
+                <Button variant="primary">Add to Planner</Button>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default ClassCard;
