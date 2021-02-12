71e387ef2442c9e07510e641c7bc81f84c35c9b5
latishasumardy <latishasumardy@gmail.com>
2021-02-11 16:31:49 -0800

    attempt to format cards into columns

--- src/App.js
+++ src/App.js
@@ -2,24 +2,46 @@ import React from 'react';
 
 import Menu from './components/navbar2.js';
 import Home from './components/Home.js';
-import ClassCard from './classcard.js';
-import './App.css'
+import ClassCardGroup from './classcard.js';
+import MoreDeets from './test.js';
+//import './App.css'
 
 function App() {
+    const classList = [{className:"Class 1",
+                            classType: "Lower Division Course",
+                            classDescription: "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                            prereqs: ["prereq 1", "prereq 2"]},
+                       {className: "Class 2",
+                            classType: "Upper Division Course",
+                            classDescription: "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                            prereqs:[]},
+                       {className:"Class 1",
+                            classType: "Lower Division Course",
+                            classDescription: "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                            prereqs: ["prereq 1", "prereq 2"]},
+                       {className: "Class 2",
+                            classType: "Upper Division Course",
+                            classDescription: "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                            prereqs:[]},
+                       {className:"Class 1",
+                            classType: "Lower Division Course",
+                            classDescription: "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                            prereqs: ["prereq 1", "prereq 2"]},
+                       {className: "Class 2",
+                            classType: "Upper Division Course",
+                            classDescription: "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                            prereqs:[]},
+                       {className:"Class 1",
+                            classType: "Lower Division Course",
+                            classDescription: "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                            prereqs: ["prereq 1", "prereq 2"]},
+                       {className: "Class 2",
+                            classType: "Upper Division Course",
+                            classDescription: "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                            prereqs:[]}];
     return(
         <div>
-            <ClassCard
-                className="Class 1"
-                classType = "Lower Division Course"
-                classDescription = "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
-                prereqs = {["prereq 1", "prereq 2"]}
-            />
-            <ClassCard
-                className="Class 2"
-                classType = "Upper Division Course"
-                classDescription = "Course Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
-                prereqs = {[]}
-            />
+            <MoreDeets/>
         </div>
     );
 }
@@ -28,4 +50,4 @@ export default App;
 
 {/*<div className="App">
     <div className="HomeModule"><Home /></div>
-</div>*/}
+</div><ClassCardGroup classList = {classList}/>*/}
--- src/classcard.js
+++ src/classcard.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Button from "react-bootstrap/Button";
 import {Card} from 'react-bootstrap';
 import {ListGroup} from 'react-bootstrap';
+import CardDeck from 'react-bootstrap/CardDeck';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import "./classcard.css";
 
 function PrereqList(props) {
@@ -26,7 +30,7 @@ function PrereqList(props) {
 function ClassCard(props) {
 
     return(
-        <Card style={{ width: '40rem', margin: '1.1rem'}}>
+        <Card style={{ width: '18rem', margin: '1.1rem'}} className="box">
             <Card.Header>{props.className}</Card.Header>
             <Card.Body>
                 <div className="description">
@@ -41,4 +45,43 @@ function ClassCard(props) {
     );
 }
 
-export default ClassCard;
+function ClassCardGroup(props) {
+    var listItems = [];
+
+    for (var i = 0; i < props.classList.length; i++) {
+        listItems.push(<ClassCard className = {props.classList[i].className}
+                                  classType = {props.classList[i].classType}
+                                  classDescription = {props.classList[i].classDescription}
+                                  prereqs = {props.classList[i].prereqs}/>);
+
+    }
+    const renderCard = (card, index) => {
+        return (
+            <Card style={{ width: '18rem'}} key={index} className="box">
+                <Card.Header>{card.className}</Card.Header>
+                <Card.Body>
+                    <div className="description">
+                        <Card.Subtitle style={{marginBottom: '1.1rem'}}>{card.classType}</Card.Subtitle>
+                        <Card.Text style={{ fontSize: '1.1rem'}}>{card.classDescription}</Card.Text>
+                    </div>
+                    <Card.Subtitle>Prerequisites:</Card.Subtitle>
+                    <PrereqList prereqs = {card.prereqs}/>
+                    <Button variant="primary">Add to Planner</Button>
+                </Card.Body>
+            </Card>
+        );
+    };
+
+
+    return(
+
+            <div className = "grid">
+                {props.classList.map(renderCard)}
+            </div>
+
+    );
+}
+
+
+
+export default ClassCardGroup;
