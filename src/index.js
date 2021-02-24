cfa24ca5c4e8cb4c300cf65637461e30397b364e
latishasumardy <latishasumardy@gmail.com>
2021-02-24 00:45:24 -0800

    revert index.js back to App

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ClassCardGroup from './components/classcard.js';
-
 
 ReactDOM.render(
   <React.StrictMode>
-    <ClassCardGroup />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
