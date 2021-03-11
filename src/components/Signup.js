f183da64ea25321c293fc8b9fb3a01d5b0ab0fae
Kelly Zheng <kellywzheng@gmail.com>
2021-03-10 20:04:34 -0800

    Update Signup.js to include profile picture upload

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,29 @@
-import React, { useContext, useCallback } from "react";
+import React, { useContext, useCallback, useState } from "react";
 import { withRouter } from "react-router";
 import app from "../base";
 import firebase from 'firebase';
 import './Signup.css';
 import Logo from './Logo.js';
+import { storage } from "../base.js"
 
 var countRef;
+var img;
 
 const Signup = ({ history }) => {
   var database = firebase.database();
+  const [image, setImage] = useState(null);
 
+  const handleChange = useCallback(async event => {
+    event.preventDefault();
+    if (event.target.files[0]) {
+      img = (event.target.files[0]);
+    }
+  }, []);
+  
   const handleSignUp = useCallback(async event => {
     event.preventDefault();
     const { email, password, name, year, major } = event.target.elements;
+
     try {
       await app
         .auth()
@@ -25,6 +36,7 @@ const Signup = ({ history }) => {
           database.ref("prereqs/" + res.user.uid).set({prereqs: "N/A"})
           database.ref("classplanner/" + res.user.uid).set({classPlanner: "N/A"})
           database.ref("emails/" + res.user.uid).set({ email: email.value })
+          storage.ref("images/" + res.user.uid).put(img);
           countRef = database.ref("userCount/")
           countRef.transaction(function (currUsers) {
             return (currUsers || 0) + 1;
@@ -35,11 +47,13 @@ const Signup = ({ history }) => {
     } catch (error) {
       alert(error);
     }
-  }, [history]);
+  }, [history, image]);
 
   return (
     <div className="Home Signup">
       <Logo />
+        <p>Sign up with a profile picture, name, email, graduation year, major, and password</p>
+        <input type="file" onChange={handleChange} />
         <form onSubmit={handleSignUp}>
             <input name="name" type="name" placeholder="Full Name" />
             <input name="email" type="email" placeholder="Email" />
