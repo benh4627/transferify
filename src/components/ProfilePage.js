2deccceb1bd2fab8dbf206a978dc7b82021e187f
latishasumardy <latishasumardy@gmail.com>
2021-03-11 18:34:45 -0800

    tried to work on dragndrop

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -10,6 +10,11 @@ var year;
 var major;
 var count;
 var myclass;
+var completedPrereqs;
+var class1;
+var class2;
+var class3;
+var class4;
 
 function Planner() {
   var ret = [];
@@ -30,9 +35,12 @@ function Planner() {
     console.log("class list: ", classList);
 
     for (var i = 0; i < classList.length; i++) {
+        var newid = "planCard".concat(i+1);
       ret.push(
-        <div draggable="true" class="planCard">     
-          {classList[i]}
+        <div class = "droptarget">
+            <div draggable="true" class="planCard" >
+              {classList[i]}
+            </div>
         </div>
       );
     }
@@ -41,14 +49,25 @@ function Planner() {
   return ret;
 }
 
+function Dropzone() {
+    return(
+        <div>
+            <div id="class12" class="droptarget"></div>
+            <div id="class12" class="droptarget"></div>
+            <div id="class34" class="droptarget"></div>
+            <div id="class34" class="droptarget"></div>
+        </div>
+    );
+}
+
 const ProfilePage = () => {
   const {currentUser} = useContext(AuthContext);
   var database = firebase.database();
-  
+
   const [image, setImage] = useState(null);
   const [url, setUrl] = useState("");
   const [progress, setProgress] = useState(0);
-  
+
   const handleChange = e => {
     if (e.target.files[0]) {
       setImage(e.target.files[0]);
@@ -78,11 +97,11 @@ const ProfilePage = () => {
       }
     );
   };
-  
-   useEffect(() => { 
+
+   useEffect(() => {
         const imgRef = storage.ref("images/" + currentUser.uid);
         const imgUrl = imgRef.getDownloadURL().then(url => {setUrl(url)});
-    
+
         const yearRef = database.ref("years/" + currentUser.uid);
         yearRef.on("value", (data) => {
             var yearData = data.val();
@@ -96,7 +115,7 @@ const ProfilePage = () => {
             var key = Object.keys(majorData);
             major = majorData[key];
         });
-    
+
         const countRef = database.ref("userCount/");
         countRef.on("value", (snap) => {
             count = snap.val();
@@ -108,13 +127,43 @@ const ProfilePage = () => {
             var key = Object.keys(classData);
             myclass = classData[key];
         });
+        const prereqsRef = database.ref("prereqs/" + currentUser.uid);
+        prereqsRef.on("value", (data) => {
+            var prereqsData = data.val();
+            var key = Object.keys(prereqsData);
+            completedPrereqs = prereqsData[key];
+        });
+        const class1Ref = database.ref("class1/" + currentUser.uid);
+        class1Ref.on("value", (data) => {
+            var class1Data = data.val();
+            var key = Object.keys(class1Data);
+            class1 = class1Data[key];
+        });
+        const class2Ref = database.ref("class2/" + currentUser.uid);
+        class2Ref.on("value", (data) => {
+            var class2Data = data.val();
+            var key = Object.keys(class2Data);
+            class2 = class2Data[key];
+        });
+        const class3Ref = database.ref("class3/" + currentUser.uid);
+        class3Ref.on("value", (data) => {
+            var class3Data = data.val();
+            var key = Object.keys(class3Data);
+            class3 = class3Data[key];
+        });
+        const class4Ref = database.ref("class4/" + currentUser.uid);
+        class4Ref.on("value", (data) => {
+            var class4Data = data.val();
+            var key = Object.keys(class4Data);
+            class4 = class4Data[key];
+        });
     }, []);
 
-  return ( 
+  return (
     <div>
         <div className = "profileInfoCard">
             <h1 class='profileTitle'>Student Information</h1>
- 
+
             <div class = "Pic">
                 <img src={url ? url : blankPic} alt="studentPic" />
             </div>
@@ -128,7 +177,7 @@ const ProfilePage = () => {
               <label>Major: {major ? major : "Retrieving data"}</label>
              </form>
 
-      
+
             <div className = "uploadPhoto">
                 <input type="file" onChange={handleChange} />
                 <progress value={progress} max="100" />
@@ -136,15 +185,16 @@ const ProfilePage = () => {
                 <button onClick={handleUpload}>Upload</button>
             </div>
         </div>
-        
-        <div class='profilePlan'> 
+
+        <div class='profilePlan'>
             <h1 class='planTitle'>Planning Schedule</h1>
+                <div>Completed Prereqs: {completedPrereqs} </div>
                 <div class='planner'>
                   <Planner/>
                 </div>
             </div>
     </div>
-    
+
   );
 };
 
