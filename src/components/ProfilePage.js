be6b317422f03b340fec80b8124b78fbc2f9addc
Grant <grant.j.wang@gmail.com>
2021-03-11 20:53:06 -0800

    bug fix 2

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -214,29 +214,29 @@ const ProfilePage = () => {
   return (
     <div>
         <div className = "profileInfoCard">
-            <h1 class='profileTitle'>Student Information</h1>
+            <h1 className='profileTitle'>Student Information</h1>
 
             <div class = "Pic">
                 <img src={url ? url : blankPic} alt="studentPic" />
             </div>
-            <form class = "profileLabels">
-              <label class="setProfile">Name: {name ? name : "Retrieving data"}</label>
-              <label class="editProfile">Name:    </label><input id="newName" type='text' class="editProfile" placeholder={name ? name : "Retrieving data"}></input>
+            <form className = "profileLabels">
+              <label className="setProfile">Name: {name ? name : "Retrieving data"}</label>
+              <label className="editProfile">Name:    </label><input id="newName" type='text' className="editProfile" placeholder={name ? name : "Retrieving data"}></input>
               <br></br>
               
               <label >Email: {currentUser.email}</label>
               <br></br>
 
-              <label class="setProfile">Graduation Year: {year ? year : "Retrieving data"}</label>
-              <label class="editProfile">Graduation Year:  </label><input id="newYear" type='text' class="editProfile" placeholder={year ? year : "Retrieving data..."}></input>
+              <label className="setProfile">Graduation Year: {year ? year : "Retrieving data"}</label>
+              <label className="editProfile">Graduation Year:  </label><input id="newYear" type='text' className="editProfile" placeholder={year ? year : "Retrieving data..."}></input>
               <br></br>
 
-              <label class="setProfile">Major: {major ? major : "Retrieving data"}</label>
-              <label class="editProfile">Major:    </label><input id="newMajor" type='text' class="editProfile" placeholder={major ? major : "Retrieving data..."}></input>
+              <label className="setProfile">Major: {major ? major : "Retrieving data"}</label>
+              <label className="editProfile">Major:    </label><input id="newMajor" type='text' className="editProfile" placeholder={major ? major : "Retrieving data..."}></input>
               <br/>
               
-              <button type="button" id="editButton" class="editButton" onClick={EditProfile}>Edit</button>
-              <button type="button" id="submitButton" class="submitButton" onClick={UpdateProfile}>Submit</button>
+              <button type="button" id="editButton" className="editButton" onClick={EditProfile}>Edit</button>
+              <button type="button" id="submitButton" className="submitButton" onClick={UpdateProfile}>Submit</button>
              </form>
 
 
@@ -248,10 +248,10 @@ const ProfilePage = () => {
             </div>
         </div>
 
-        <div class='profilePlan'>
-            <h1 class='planTitle'>Planning Schedule</h1>
+        <div className='profilePlan'>
+            <h1 className='planTitle'>Planning Schedule</h1>
                 <div>Completed Prereqs: {completedPrereqs} </div>
-                <div class='planner'>
+                <div className='planner'>
                   <Planner/>
                 </div>
             </div>
